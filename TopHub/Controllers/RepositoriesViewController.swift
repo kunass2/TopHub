4f94f0ee76df1bb4a6382e9aa4ec72a0ab10e8c9
Bartłomiej Semańczyk <bartekss2@icloud.com>
2017-08-09 10:54:21 +0200

    Setup project and basic file structure.

---
+++ TopHub/Controllers/RepositoriesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RepositoriesViewController.swift
+//  TopHub
+//
+//  Created by Bartłomiej Semańczyk on 09/08/2017.
+//  Copyright © 2017 Bartłomiej Semańczyk. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+    
+    //MARK: - Class Methods
+    
+    //MARK: - Initialization
+    
+    //MARK: - Deinitialization
+    
+    //MARK: - Actions
+    
+    //MARK: - Open
+    
+    //MARK: - Internal
+    
+    //MARK: - Private
+    
+    //MARK: - Overridden
+}
---
+++ TopHub/Controllers/UsersViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UsersViewController.swift
+//  TopHub
+//
+//  Created by Bartłomiej Semańczyk on 09/08/2017.
+//  Copyright © 2017 Bartłomiej Semańczyk. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+    
+    //MARK: - Class Methods
+    
+    //MARK: - Initialization
+    
+    //MARK: - Deinitialization
+    
+    //MARK: - Actions
+    
+    //MARK: - Open
+    
+    //MARK: - Internal
+    
+    //MARK: - Private
+    
+    //MARK: - Overridden
+}
